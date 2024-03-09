43da206f3ec4e85887f5b2081880081dbd9c95b0
DjCr4y0n <cray.kox1234@gmail.com>
2024-03-09 18:00:35 +0100

    Modify object check state to change boolean gameEnded to go true if game is ended and then when it's true reset board and score

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function Gameboard()
     const rows = 3;
     const columns = 3;
 
-    const getRows = () => rows;
-
     const gameboard = [];
 
     for(let i = 0; i < rows; i++)
@@ -34,9 +32,17 @@ function Gameboard()
         console.log(boardWithCellValues);
     }
 
+    const clearBoard = () => {
+        for(let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                gameboard[i][j].addMarker(0); // Reset each spot to initial state (0)
+            }
+        }
+    }
+
     
 
-    return {getBoard, selectSpot, printBoard, getRows}
+    return {getBoard, selectSpot, printBoard, clearBoard}
 }
 
 function Spot()
@@ -58,6 +64,7 @@ function Spot()
 function GameController(playerOne = "Player One", playerTwo = "Player Two")
 {
     const gameboard = Gameboard();
+    let gameEnded = false; 
 
     const players = [
         {
@@ -91,12 +98,20 @@ function GameController(playerOne = "Player One", playerTwo = "Player Two")
         console.log(`Putting ${getActivePlayer().nick}'s marker on row ${row} and column ${column}`);
         gameboard.selectSpot(row,column,getActivePlayer().marker);
 
+        gameboard.printBoard();
+
         checkState();
+        if (gameEnded) {
+            console.log("Game has already ended.");
+            gameEnd();
+            return;
+        }
         switchPlayerTurn();
         printNewRound();
     }
 
     const checkState = () => {
+        console.log(getActivePlayer().marker);
         function checkValue(value)
         {
             return value == getActivePlayer().marker;
@@ -104,45 +119,47 @@ function GameController(playerOne = "Player One", playerTwo = "Player Two")
         
         if (gameboard.getBoard()[0].every(checkValue)) {
             console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+            gameEnded = true;
         }
         else if (gameboard.getBoard()[1].every(checkValue)) {
             console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+            gameEnded = true;
         }
         else if (gameboard.getBoard()[2].every(checkValue)) {
-            console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+            console.log(`Game Winner1 is ${getActivePlayer().nick}`);
+            gameEnded = true;
         }
-        else if (gameboard.getBoard()[0].every(checkValue)) {
-            console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+        else if (gameboard.getBoard()[0][0] === gameboard.getBoard()[1][0] && gameboard.getBoard()[1][0] === gameboard.getBoard()[2][0] && gameboard.getBoard()[0][0]!=0) {
+            console.log(`Game Winner3 is ${getActivePlayer().nick}`);
+            gameEnded = true;
         }
-        else if (gameboard.getBoard()[0][0] === gameboard.getBoard()[1][0] && gameboard.getBoard()[1][0] === gameboard.getBoard()[2][0]) {
-            console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+        else if (gameboard.getBoard()[0][1] === gameboard.getBoard()[1][1] && gameboard.getBoard()[1][1] === gameboard.getBoard()[2][1] && gameboard.getBoard()[0][1]!=0) {
+            console.log(`Game Winner4 is ${getActivePlayer().nick}`);
+            gameEnded = true;
         }
-        else if (gameboard.getBoard()[0][1] === gameboard.getBoard()[1][1] && gameboard.getBoard()[1][1] === gameboard.getBoard()[2][1]) {
-            console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+        else if (gameboard.getBoard()[0][2] === gameboard.getBoard()[1][2] && gameboard.getBoard()[1][2] === gameboard.getBoard()[2][2] && gameboard.getBoard()[0][2]!=0) {
+            console.log(`Game Winner5 is ${getActivePlayer().nick}`);
+            gameEnded = true;
         }
-        else if (gameboard.getBoard()[0][2] === gameboard.getBoard()[1][2] && gameboard.getBoard()[1][2] === gameboard.getBoard()[2][2]) {
-            console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+        else if (gameboard.getBoard()[0][0] === gameboard.getBoard()[1][1] && gameboard.getBoard()[1][1] === gameboard.getBoard()[2][2] && gameboard.getBoard()[0][0]!=0) {
+            console.log(`Game Winner6 is ${getActivePlayer().nick}`);
+            gameEnded = true;
         }
-        else if (gameboard.getBoard()[0][0] === gameboard.getBoard()[1][1] && gameboard.getBoard()[1][1] === gameboard.getBoard()[2][2]) {
-            console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
-        }
-        else if (gameboard.getBoard()[0][2] === gameboard.getBoard()[1][1] && gameboard.getBoard()[1][1] === gameboard.getBoard()[2][0]) {
-            console.log(`Game Winner is ${getActivePlayer().nick}`);
-            return;
+        else if (gameboard.getBoard()[0][2] === gameboard.getBoard()[1][1] && gameboard.getBoard()[1][1] === gameboard.getBoard()[2][0] && gameboard.getBoard()[0][2]!=0) {
+            console.log(`Game Winner7 is ${getActivePlayer().nick}`);
+            gameEnded = true;
         }
         else if(counter >= 9)
         { 
             console.log("It's a tie!");
-            return;
+            gameEnded = true;
         }
+    }
+
+    const gameEnd = () =>
+    {
+        gameboard.clearBoard();
+        counter = 0;
     }
 
     return {playRound};
